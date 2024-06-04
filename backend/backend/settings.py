c17f449df41fcbf877789cbc07f314c0f1d8c26c
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-06-04 06:42:58 +0500

    jwt access change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
 
 # DRF JWT Configuration
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
